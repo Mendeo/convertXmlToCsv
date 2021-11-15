779ae98763c4f70b5260bbe2bb87e513a52577ae
Themen <thesolve@mail.ru>
2021-11-15 09:45:57 +0300

    Первый коммит

---
+++ MainModule.vb
@@ -0,0 +1,44 @@
+﻿Option Strict On
+Option Explicit On
+
+Imports System.Xml
+Module MainModule
+
+	Sub Main(cargs() As String)
+		Dim outputFolder As String = My.Application.Info.DirectoryPath
+		If cargs Is Nothing OrElse cargs.Length = 0 OrElse String.IsNullOrEmpty(cargs(0)) Then
+			Console.WriteLine("You need to set the input file")
+			Return
+		End If
+		Dim inputFile As String = cargs(0)
+		'inputFile = "data.xml"
+
+		Dim xDoc As New XmlDocument()
+		xDoc.Load(inputFile)
+		Dim xRoot As XmlElement = xDoc.DocumentElement
+		If xRoot Is Nothing Then
+			Console.WriteLine("Cannot find root element")
+			Return
+		End If
+		Dim row As New List(Of String)
+		Dim sw As New IO.StreamWriter(IO.Path.Combine(IO.Path.GetPathRoot(inputFile), IO.Path.GetFileNameWithoutExtension(inputFile) & ".csv"))
+
+		For Each x1 As XmlElement In xRoot
+			For Each x2 As XmlElement In x1
+				row.Add(x2.Name)
+			Next
+			Exit For
+		Next
+		sw.WriteLine(String.Join(";", row))
+		For Each xRow As XmlElement In xRoot
+			row.Clear()
+			For Each xFiled As XmlElement In xRow
+				row.Add(xFiled.InnerText)
+			Next
+			sw.WriteLine(String.Join(";", row))
+		Next
+		sw.Flush()
+		sw.Close()
+	End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("covertXmlToCsv")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("covertXmlToCsv")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("291fddb0-0f51-4ec0-88a5-28e9b1e41f21")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
